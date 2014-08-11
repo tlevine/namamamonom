e0978cb3863cb7bb3eadf9a4f134caefec5e61b4
Thomas Levine <_@thomaslevine.com>
2014-08-11 11:48:31 +0000

    arg parser

--- namamamonom/main.py
+++ namamamonom/main.py
@@ -1,12 +1,20 @@
+import argparse
+
 from namamamonom.columns import read
 from namamamonom.name import is_name
 
+parser = argparse.ArgumentParser()
+parser.add_argument('filename', nargs = '+')
+
 def main():
+    args = parser.parse_args()
+
     import sys, csv
     writer = csv.writer(sys.stdout)
-    writer.writerow(('name', 'is_name'))
+    writer.writerow(('filename', 'name', 'is_name'))
 
-    columns = read('/home/tlevine/csv-files/https/data.cityofnewyork.us/resource/feu5-w2e2.csv')
-    for name, values in columns.items():
-        result = 'name' in name.lower() and is_name(values)
-        writer.writerow((name, result))
+    for filename in args.filename:
+        columns = read(filename)
+        for name, values in columns.items():
+            result = 'name' in name.lower() and is_name(values)
+            writer.writerow((filename, name, result))
